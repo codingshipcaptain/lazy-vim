1dd01d5546bb49194534273fd083c5d36e9835e3
Andrew Callahan <andrew.callahan@velocigo.com>
2024-05-21 12:03:32 -0500

    setup tsserver for volar use

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -45,3 +45,22 @@ require("lazy").setup({
 		},
 	},
 })
+
+require("lspconfig").volar.setup({})
+
+require("lspconfig").tsserver.setup({
+	init_options = {
+		plugins = {
+			{
+				name = "@vue/typescript-plugin",
+				location = "/usr/local/lib/node_modules/@vue/typescript-plugin",
+				languages = { "javascript", "typescript", "vue" },
+			},
+		},
+	},
+	filetypes = {
+		"javascript",
+		"typescript",
+		"vue",
+	},
+})
