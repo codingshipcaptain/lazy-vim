f8eb91a0e0842580b19af4621a5334a449a71c5c
Andrew Callahan <andrew.callahan@velocigo.com>
2024-05-21 10:13:16 -0500

    Update for lualine plugin config file- Just defaults at the moment

--- init.lua
+++ init.lua
@@ -1,4 +1,7 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+require("config/lualine-config")
+
+-- Vim settings for visuals ie tab spacing
 vim.o.tabstop = 4 -- A TAB character looks like 4 spaces
 vim.o.shiftwidth = 4 -- Number of spaces inserted when indenting
