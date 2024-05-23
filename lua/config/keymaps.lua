7901332b1d9bd1462fce0f3969cebd53a3bbbd4b
Andrew Callahan <andrew.callahan@velocigo.com>
2024-05-23 09:57:27 -0500

    comments on keymapping

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,6 +1,13 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Default Lazy-Vim keymaps by module here
+-- http://www.lazyvim.org/keymaps
+
+-- Navigate backward through open buffers
 vim.keymap.set("n", "<C-z>", ":bp<CR>")
+-- Navigate forward through open buffers
 vim.keymap.set("n", "<C-x>", ":bn<CR>")
+-- Toggle highlight on search results
 vim.keymap.set("n", "<C-c>", ":set hlsearch!<CR>")
