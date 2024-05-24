e30a3170e4916d938a364dbdd77e8b4b6417d88a
Andrew Callahan <andrew.callahan@velocigo.com>
2024-05-24 10:05:51 -0500

    recommended setting on term colors

--- init.lua
+++ init.lua
@@ -3,6 +3,7 @@ require("config.lazy")
 require("config.lualine-config")
 require("config.onedark")
 
+vim.opt.termguicolors = true
 -- Vim settings for visuals ie tab spacing
 vim.o.tabstop = 4 -- A TAB character looks like 4 spaces
 vim.o.shiftwidth = 4 -- Number of spaces inserted when indenting
