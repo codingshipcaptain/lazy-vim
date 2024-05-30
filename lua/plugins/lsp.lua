86cabf030e4ecfe8d8d1fc6c887b574c401cf5e0
Andrew Callahan <andrew.callahan@velocigo.com>
2024-05-30 14:55:25 -0500

    trouble.nvim v3 integration

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -73,4 +73,32 @@ return {
             ]]
 		},
 	},
+	{
+		"folke/trouble.nvim",
+		-- opts will be merged with the parent spec
+		opts = { use_diagnostic_signs = true },
+	},
+	{
+		"nvim-lualine/lualine.nvim",
+		opts = function(_, opts)
+			local trouble = require("trouble")
+			if not trouble.statusline then
+				LazyVim.error("You have enabled the **trouble-v3** extra,\nbut still need to update it with `:Lazy`")
+				return
+			end
+
+			local symbols = trouble.statusline({
+				mode = "symbols",
+				groups = {},
+				title = false,
+				filter = { range = true },
+				format = "{kind_icon}{symbol.name:Normal}",
+				hl_group = "lualine_c_normal",
+			})
+			table.insert(opts.sections.lualine_c, {
+				symbols.get,
+				cond = symbols.has,
+			})
+		end,
+	},
 }
