c20bbeb8421703768cb8955e0c95490e29b5d86e
Andrew Callahan <andrew.callahan@velocigo.com>
2024-07-22 08:04:43 -0500

    no loaded perl provider and softtabstop = 4

--- init.lua
+++ init.lua
@@ -6,4 +6,6 @@ require("config.onedark")
 vim.opt.termguicolors = true
 -- Vim settings for visuals ie tab spacing
 vim.o.tabstop = 4 -- A TAB character looks like 4 spaces
+vim.o.softtabstop = 4 -- Seriously, all tabs need to be 4 ppl
 vim.o.shiftwidth = 4 -- Number of spaces inserted when indenting
+vim.o.loaded_perl_provider = 0
